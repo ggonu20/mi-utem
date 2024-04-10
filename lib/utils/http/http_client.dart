8d86ea80e89ce09a35b31c6a3678b94fbac61393
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2024-04-10 08:48:17 -0400

    patch: se mejora http_client * Se coloca la configuración de caché dentro de la inicialización de DioCacheManager * Se elimina 'getter' de authClient (para evitar que sea computado y sea un cliente estático)
    Signed-off-by: Francisco Solis <30329003+Im-Fran@users.noreply.github.com>

--- lib/utils/http/http_client.dart
+++ lib/utils/http/http_client.dart
@@ -12,13 +12,11 @@ class HttpClient {
   static const String productionUrl = 'https://api.exdev.cl/';
   static String url = isProduction ? productionUrl : debugUrl;
 
-  static CacheConfig cacheConfig = CacheConfig(
+  static DioCacheManager dioCacheManager = DioCacheManager(CacheConfig(
     baseUrl: url,
     defaultMaxAge: Duration(days: 7),
     defaultMaxStale: Duration(days: 60),
-  );
-
-  static DioCacheManager dioCacheManager = DioCacheManager(cacheConfig);
+  ));
 
   static Dio httpClient = Dio(BaseOptions(baseUrl: url))..interceptors.addAll([
     HeadersInterceptor(),
@@ -27,7 +25,7 @@ class HttpClient {
     dioCacheManager.interceptor,
   ]);
 
-  static Dio get authClient => httpClient..interceptors.add(AuthInterceptor());
+  static Dio authClient = httpClient..interceptors.add(AuthInterceptor());
 
   static InterceptorsWrapper _errorInterceptor = InterceptorsWrapper(
     onError: (DioError err, ErrorInterceptorHandler handler) {
