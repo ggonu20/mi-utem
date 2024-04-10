4d74b42cdaa84cdbf6b1d967b2a20bf74929da12
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2024-04-10 08:49:16 -0400

    patch: se agrega resizeToAvoidBottomInset al gradient_background.dart
    Signed-off-by: Francisco Solis <30329003+Im-Fran@users.noreply.github.com>

--- lib/widgets/gradient_background.dart
+++ lib/widgets/gradient_background.dart
@@ -3,14 +3,17 @@ import 'package:mi_utem/themes/theme.dart';
 
 class GradientBackground extends StatelessWidget {
   final Widget child;
+  final bool resizeToAvoidBottomInset;
 
   const GradientBackground({
     super.key,
     required this.child,
+    this.resizeToAvoidBottomInset = true,
   });
 
   @override
   Widget build(BuildContext context) => Scaffold(
+    resizeToAvoidBottomInset: resizeToAvoidBottomInset,
     body: Stack(
       children: [
         Container(
