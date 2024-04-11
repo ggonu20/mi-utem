3434521eff29704163803fe68c0c1e01d95adef6
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2024-04-11 09:51:11 -0400

    patch: ahora default_network_image es constante
    Signed-off-by: Francisco Solis <30329003+Im-Fran@users.noreply.github.com>

--- lib/widgets/default_network_image.dart
+++ lib/widgets/default_network_image.dart
@@ -5,54 +5,52 @@ import 'package:mi_utem/widgets/image_view_screen.dart';
 class DefaultNetworkImage extends StatelessWidget {
   final String? url;
 
-  DefaultNetworkImage({
+  const DefaultNetworkImage({
     this.url,
   });
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: double.infinity,
-      width: double.infinity,
-      child: CachedNetworkImage(
-        imageUrl: "$url",
-        imageBuilder: (context, imageProvider) => GestureDetector(
-          onTap: () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => ImageViewScreen(imageProvider: imageProvider))),
-          child: Container(
-            height: double.infinity,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              image: DecorationImage(
-                image: imageProvider,
-                fit: BoxFit.cover,
-              ),
+  Widget build(BuildContext context) => Container(
+    height: double.infinity,
+    width: double.infinity,
+    child: CachedNetworkImage(
+      imageUrl: "$url",
+      imageBuilder: (context, imageProvider) => GestureDetector(
+        onTap: () => Navigator.push(context, MaterialPageRoute(builder: (ctx) => ImageViewScreen(imageProvider: imageProvider))),
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            image: DecorationImage(
+              image: imageProvider,
+              fit: BoxFit.cover,
             ),
           ),
         ),
-        placeholder: (context, url) => Container(
-          height: double.infinity,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.grey[300],
-          ),
-          child: Center(
-            child: Icon(Icons.photo),
-          ),
+      ),
+      placeholder: (context, url) => Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.grey[300],
         ),
-        errorWidget: (context, url, error) => Container(
-          height: double.infinity,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.grey[300],
-          ),
-          child: Center(
-            child: Icon(Icons.photo),
-          ),
+        child: Center(
+          child: Icon(Icons.photo),
         ),
       ),
-    );
-  }
+      errorWidget: (context, url, error) => Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.grey[300],
+        ),
+        child: Center(
+          child: Icon(Icons.photo),
+        ),
+      ),
+    ),
+  );
 }
