f21b964e70fb977864719359df6718e36277054c
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2024-05-23 10:49:52 -0400

    patch: se agregan datos a sentry para la identificación del usuario, además de métricas.
    Signed-off-by: Francisco Solis <30329003+Im-Fran@users.noreply.github.com>

--- lib/services/analytics_service.dart
+++ lib/services/analytics_service.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter_uxcam/flutter_uxcam.dart';
 import 'package:mi_utem/models/carrera.dart';
 import 'package:mi_utem/models/user/user.dart';
+import 'package:mi_utem/utils/string_utils.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 class AnalyticsService {
@@ -24,17 +28,20 @@ class AnalyticsService {
       await FlutterUxcam.setUserProperty("last_name", user.apellidos!);
     }
 
-    await Sentry.configureScope((scope) => scope.setUser(
-      SentryUser(
-        id: user.correoUtem,
-        email: user.correoPersonal,
-        name: user.nombreCompleto,
-        data: {
-          "rut": user.rut?.toString(),
-        },
-        ipAddress: "{{auto}}",
-      ),
-    ));
+    final correoUtem = user.correoUtem;
+
+    await Sentry.configureScope((scope) => scope.setUser(SentryUser(
+      id: correoUtem,
+      email: user.correoPersonal,
+      name: capitalize(user.nombreCompleto),
+      data: {
+        "rut": user.rut?.toString(),
+        "usuario": user.username,
+        "backend_uid": correoUtem != null ? md5.convert(utf8.encode(correoUtem)) : "desconocido",
+        "perfiles": user.perfiles,
+      },
+      ipAddress: "{{auto}}",
+    )));
   }
 
   static Future<void> setCarreraToUser(Carrera carrera) async {
@@ -47,6 +54,14 @@ class AnalyticsService {
       await FirebaseAnalytics.instance.setUserProperty(name: "estadoCarreraActiva", value: carrera.estado!);
       await FlutterUxcam.setUserProperty("estadoCarreraActiva", carrera.estado!);
     }
+
+    await Sentry.configureScope((scope) => scope.setUser(scope.user?.copyWith(
+      data: {
+        ...?scope.user?.data,
+        "carrera": carrera.nombre,
+        "estado_carrera": carrera.estado,
+      },
+    )));
   }
 
   static Future<void> removeUser() async {
@@ -56,15 +71,14 @@ class AnalyticsService {
   }
 
   static Future<void> logEvent(String name, {Map<String, dynamic>? parameters}) async {
-    await FirebaseAnalytics.instance.logEvent(
-      name: name,
-      parameters: parameters,
-    );
+    await FirebaseAnalytics.instance.logEvent(name: name, parameters: parameters);
 
     if (parameters != null) {
       await FlutterUxcam.logEventWithProperties(name, parameters);
     } else {
       await FlutterUxcam.logEvent(name);
     }
+
+    Sentry.metrics().increment(name, tags: parameters?.map((key, value) => MapEntry(key, value.toString())));
   }
 }
