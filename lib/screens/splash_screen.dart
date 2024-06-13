a714af382042af714c56d5a5ed2be3a21735b325
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2024-06-13 12:27:28 -0400

    patch: se utiliza nueva función de #isOffline
    Signed-off-by: Francisco Solis <30329003+Im-Fran@users.noreply.github.com>

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,15 +2,13 @@ import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:mi_utem/config/constants.dart';
-import 'package:mi_utem/config/logger.dart';
 import 'package:mi_utem/models/preferencia.dart';
 import 'package:mi_utem/screens/login_screen/login_screen.dart';
 import 'package:mi_utem/screens/main_screen.dart';
 import 'package:mi_utem/screens/onboarding/welcome_screen.dart';
 import 'package:mi_utem/services/analytics_service.dart';
 import 'package:mi_utem/services/auth_service.dart';
-import 'package:mi_utem/utils/http/http_client.dart';
+import 'package:mi_utem/utils/http/functions.dart';
 import 'package:mi_utem/widgets/loading/loading_dialog.dart';
 import 'package:mi_utem/widgets/snackbar.dart';
 import 'package:package_info_plus/package_info_plus.dart';
@@ -70,17 +68,9 @@ class _SplashScreenState extends State<SplashScreen> {
                     animation: "default",
                     callback: (String val) async {
                       showLoadingDialog(context);
-                      // Revisar si tenemos conexión a internet
-                      bool offlineMode = (await Preferencia.isOffline.get(defaultValue: "false", guardar: true)) == "true";
-                      try {
-                        final response = await HttpClient.httpClient.head(apiUrl);
-                        offlineMode = response.statusCode != 200;
-                      } catch (e) {
-                        logger.e("[SplashScreen]: Error al conectar con la API", e);
-                        offlineMode = true;
-                      }
 
-                      await Preferencia.isOffline.set(offlineMode.toString());
+                      // Revisar si tenemos conexión a internet
+                      bool offlineMode = await isOffline();
                       final user = await _authService.getUser();
 
                       if(offlineMode) {
